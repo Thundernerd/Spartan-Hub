c919e9b5401b4eb99bf5f895057791ef04cde50a
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-03-12 01:53:17 +0100

    Bit of profile UI

--- SpartanHub/scripts/ui-handler.js
+++ SpartanHub/scripts/ui-handler.js
@@ -1,10 +1,19 @@
 function doProfileUI() {
-    appendLine(format("{0} - {1}", profile.name, profile.tag));
-    appendLine(format("<img src=\"{0}\"></img>", profile.imageUrl));
-    appendLine(format("{0} - {1}/{2} | {3}%", profile.rank, profile.currentXp, profile.maximumXp, profile.levelPercentage * 100));
-    appendLine(format("Arena: W:{0} L:{1} R: {2}", profile.arenaWins, profile.arenaLosses, profile.arenaWinLossRatio));
-    appendLine(format("Warzone: W:{0} L:{1} R: {2}", profile.warzoneWins, profile.warzoneLosses, profile.warzoneWinLossRatio));
-    appendLine(format("Total: W:{0} L:{1} R: {2}", profile.totalWins, profile.totalLosses, profile.totalWinLossRatio));
+    var text = "";
+
+    text += "<section class=\"profile-wrapper\">";
+    text += format("<h2>{0}</h2> &nbsp; - {1}", profile.name, profile.tag);
+    text += format("<img src=\"{0}\"></img>", profile.imageUrl);
+    text += "</section>";
+
+    append(text);
+
+    // appendLine(format("{0} - {1}", profile.name, profile.tag));
+    // appendLine(format("<img src=\"{0}\"></img>", profile.imageUrl));
+    // appendLine(format("{0} - {1}/{2} | {3}%", profile.rank, profile.currentXp, profile.maximumXp, profile.levelPercentage * 100));
+    // appendLine(format("Arena: W:{0} L:{1} R: {2}", profile.arenaWins, profile.arenaLosses, profile.arenaWinLossRatio));
+    // appendLine(format("Warzone: W:{0} L:{1} R: {2}", profile.warzoneWins, profile.warzoneLosses, profile.warzoneWinLossRatio));
+    // appendLine(format("Total: W:{0} L:{1} R: {2}", profile.totalWins, profile.totalLosses, profile.totalWinLossRatio));
 }
 
 function doRequisitionsUI() {
@@ -14,7 +23,7 @@ function doRequisitionsUI() {
         var text = "";
 
         text = format("<section class=\"requisition-wrapper {0}\">", left == true ? "left" : "right");
-        text += format("<h2 class=\"requisition-header\">{0}</h2>", cat);
+        text += format("<h3 class=\"requisition-header\">{0}</h3>", cat);
 
         var keyCount = Object.keys(requisitions[cat]).length;
         var i = 0;
